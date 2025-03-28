9a300e90498623ea61707614b66c0048e22c832f
Aweroro Hadi <hadistic20@gmail.com>
2025-03-28 17:35:56 +0100

    feat(file-viewer): added feature to viewing pdf files

---
+++ src/app/components/FileViewer.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React from 'react';
+import { useState } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "pdfjs-dist/build/pdf.worker.min.mjs",
+    import.meta.url
+  ).toString();
+
+//   pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+interface FileViwerProps {
+    file: File | null;
+}
+
+const FileViewer = ({file}: FileViwerProps) => {
+    const [numPages, setNumPages] = useState<number | null>(null);
+
+  return (
+    <div className='overflow-auto border rounded-lg w-full h-[80vh]'
+    >
+      {file ? (
+        <Document
+        file={URL.createObjectURL(file)}
+        onLoadSuccess={({numPages}) => setNumPages(numPages)}
+        >
+            {Array.from(new Array(numPages), (el, index) => (
+                <Page key={index} pageNumber={index + 1} renderTextLayer={false} />
+            ))}
+        </Document>
+      ) : (
+        <p className='text-center text-gray-500'>No PDF uploaded</p>
+      )}
+    </div>
+  );
+};
+
+export default FileViewer;
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 'use client'
 import React from 'react'
 import { useState, useRef } from 'react'
-import PDFUploader from './components/PDFuploader'
+import PDFUploader from './components/PDFuploader';
+import FileViewer from './components/FileViewer';
 
 const Home = () => {
   const [file, setFile] = useState<File | null>(null);
-
+  const pdfRef = useRef<HTMLDivElement | null>(null);
 
   return (
-    <div>
+    <div className='p-4'>
       <PDFUploader onFileSelect={setFile}/>
+      <div ref={pdfRef} className='relative'>
+        <FileViewer file={file}/>
+      </div>
     </div>
   )
 }
